ffd2b13fbae4655b5091d55c669eb0d543e5953c
AlexeyTomskikh <alexey.tomskih@gmail.com>
2017-02-05 22:06:05 +0700

    Корректировка задачи Range 1.1

--- Range/src/Range.java
+++ Range/src/Range.java
@@ -1,3 +1,5 @@
+import java.lang.*;
+
 public class Range {
 
     private double from;
@@ -24,12 +26,63 @@ public class Range {
         this.to = to;
     }
 
+    /**
+     * Функция проверки принадлежности числа диапазону.
+     *
+     * @param number принимает вещественное число
+     * @return boolean результат проверки того
+     */
+
     public boolean isInside(double number) {
 
-        if (number >= from & number <= to) {
-            return true;
+        return number >= from & number <= to;
+    }
+
+    /**
+     * Функция вычисления длины интервала.
+     *
+     * @return длина диапазона
+     */
+
+    public double lengthRange() {
+        return to - from;
+    }
+
+    /**
+     * Функция вычисляет интервал-пересечения двух интервалов.
+     *
+     * @param range объект
+     * @return если пересечения нет, функция выдаёт null, если есть, то выдаёт новый объект Range с соответствующими концами
+     */
+    public Range crossingRange(Range range) {
+
+        if (range.from >= this.from && range.from <= this.to) {
+            return new Range(range.from, this.to);
         } else {
-            return false;
+            if (range.to >= this.from && range.to <= this.to) {
+                return new Range(this.from, range.to);
+            } else {
+                return null;
+            }
         }
     }
+
+    /**
+     * Функция объединяет два интервала.
+     *
+     * @param range первый интервал
+     * @return массив из приянятых интервалов
+     */
+    public Range[] mergerRange(Range range) {
+
+        Range rangeCopy = new Range(this.from, this.to);
+        return new Range[]{new Range(rangeCopy.from, range.to)};
+    }
+
+    @Override
+    public String toString() {
+        return new StringBuilder().append("(").append(from).append(", ").append(to).append(")").toString();
+    }
+
+
 }
--- Range/src/Start.java
+++ Range/src/Start.java
@@ -1,11 +1,33 @@
-import java.util.Random;
-
 public class Start {
 
     public static void main(String[] args) {
 
-        Range range = new Range(-1, 23);
-        boolean result = range.isInside(24.0);
-        System.out.println(result);
+        Range range1 = new Range(1.0, 6.0);
+        Range range2 = new Range(7.0, 12.0);
+
+        // Проверяем функцию на принадлежность числа "x" диапазону.
+        int x = 12;
+        System.out.println("1. Число " + x + (range1.isInside(x) ? " принадлежит диапазону " + range1 : " не принадлежит диапазону " + range1));
+        System.out.println("   Число " + x + (range2.isInside(x) ? " принадлежит диапазону " + range2 : " не принадлежит диапазону " + range2));
+
+        // Проверяем функцию вычисления длины интервала.
+
+        System.out.println("2. Длина интервала: " + range1 + " = " + range1.lengthRange());
+        System.out.println("   Длина интервала: " + range2 + " = " + range2.lengthRange());
+
+
+        // Проверяем функцию вычисления интервала-пересечения двух интервалов
+
+        Range resultRange = range1.crossingRange(range2);
+        System.out.println(resultRange == null ? "3. Интервалы не пересекаются" : "3. Интервал пересечения отрезков" + range1 + " и " + range2 + " = " + resultRange);
+
+        // Проверяем функцию объединения двух интервалов
+
+        Range[] mergedRange = range1.mergerRange(range2);
+
+        for (int i = 0; i <= mergedRange.length - 1; i++) {
+            System.out.println("4. Результатом объединения интервалов " + range1 + " и " + range2 + " является интервал " + mergedRange[i]);
+        }
+
     }
 }
