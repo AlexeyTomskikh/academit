cf5de4da912e55274ea700a2121ab14e50407c00
AlexeyTomskikh <alexey.tomskih@gmail.com>
2017-02-09 22:00:19 +0700

    Корректировка задачи Range 1.2. Дополнено пересечение, добавлена разность

--- Range/src/Range.java
+++ Range/src/Range.java
@@ -30,12 +30,11 @@ public class Range {
      * Функция проверки принадлежности числа диапазону.
      *
      * @param number принимает вещественное число
-     * @return boolean результат проверки того
+     * @return boolean результат проверки
      */
-
     public boolean isInside(double number) {
 
-        return number >= from & number <= to;
+        return number >= from && number <= to;
     }
 
     /**
@@ -43,8 +42,7 @@ public class Range {
      *
      * @return длина диапазона
      */
-
-    public double lengthRange() {
+    public double length() {
         return to - from;
     }
 
@@ -56,12 +54,19 @@ public class Range {
      */
     public Range crossingRange(Range range) {
 
-        if (range.from >= this.from && range.from <= this.to) {
+        if (range.from >= this.from && range.from <= this.to && range.to >= this.to) {  // 3..7 / 4..12   = 4..7
             return new Range(range.from, this.to);
         } else {
-            if (range.to >= this.from && range.to <= this.to) {
+            if (range.to >= this.from && range.to <= this.to && range.from <= this.from) {    //  3..5 / 1...7  = 3..5
                 return new Range(this.from, range.to);
             } else {
+                if (range.from <= this.from && range.to >= this.to) {  // 3..5 / 1...10 = 3..5
+                    return new Range(this.from, this.to);
+                } else {
+                    if (this.from <= range.from && this.to >= range.to) {
+                        return new Range(range.from, range.to);
+                    }
+                }
                 return null;
             }
         }
@@ -71,18 +76,60 @@ public class Range {
      * Функция объединяет два интервала.
      *
      * @param range первый интервал
-     * @return массив из приянятых интервалов
+     * @return массив из принятых интервалов
      */
-    public Range[] mergerRange(Range range) {
+    public Range[] unionRange(Range range) {
 
         Range rangeCopy = new Range(this.from, this.to);
-        return new Range[]{new Range(rangeCopy.from, range.to)};
+        if (rangeCopy.to > range.to && rangeCopy.from < range.from) { // если первая охватывает вторую
+            return new Range[]{new Range(rangeCopy.from, rangeCopy.to)};
+        } else {
+            if (range.from < rangeCopy.from && range.to > rangeCopy.to) { // если вторая охватывает первую
+                return new Range[]{new Range(range.from, range.to)};
+            } else {
+                return new Range[]{new Range(rangeCopy.from, range.to)};   // остальные случаи
+            }
+        }
+    }
+
+    /**
+     * Функция вычисляет разность интервалов.
+     *
+     * @param range первый интервал
+     * @return массив содержащий интервал -результат
+     */
+    public Range[] difference(Range range) {
+
+        if (this.from < range.from && this.to > range.to) { // если первое множество охватывает второе
+            Range a = new Range(this.from, range.from - 1);
+            Range b = new Range(range.to + 1, this.to);
+            return new Range[]{a, b};
+        } else {
+            if (range.from <= this.from && range.to >= this.to) { // если второе охватывает первое
+                return null;
+            } else {
+                if (this.to < range.from || this.from > range.to) { // если интервалы не пересекаются
+                    return new Range[]{this};
+                } else {
+                    if (this.from == range.from && this.to == range.to) { // если интервалы одинаковые
+                        return null;
+                    } else {
+                        if (range.from >= this.from && range.from <= this.to && range.to >= this.to) {   // если интервалы наклыдваются друг на друга
+                            Range a = new Range(this.from, range.from - 1);
+                            return new Range[]{a};
+                        }
+                    }
+
+                }
+            }
+            return null;
+        }
     }
 
     @Override
     public String toString() {
-        return new StringBuilder().append("(").append(from).append(", ").append(to).append(")").toString();
+
+        return String.format("(" + from + ", " + to + ")");
     }
-
-
 }
+
--- Range/src/Start.java
+++ Range/src/Start.java
@@ -2,32 +2,39 @@ public class Start {
 
     public static void main(String[] args) {
 
-        Range range1 = new Range(1.0, 6.0);
-        Range range2 = new Range(7.0, 12.0);
+        Range range1 = new Range(1.0, 5.0);
+        Range range2 = new Range(3.0, 7.0);
 
-        // Проверяем функцию на принадлежность числа "x" диапазону.
+        // 1. Проверяем функцию на принадлежность числа "x" диапазону.
         int x = 12;
         System.out.println("1. Число " + x + (range1.isInside(x) ? " принадлежит диапазону " + range1 : " не принадлежит диапазону " + range1));
         System.out.println("   Число " + x + (range2.isInside(x) ? " принадлежит диапазону " + range2 : " не принадлежит диапазону " + range2));
 
-        // Проверяем функцию вычисления длины интервала.
+        // 2. Проверяем функцию вычисления длины интервала.
+        System.out.println("2. Длина интервала: " + range1 + " = " + range1.length());
+        System.out.println("   Длина интервала: " + range2 + " = " + range2.length());
 
-        System.out.println("2. Длина интервала: " + range1 + " = " + range1.lengthRange());
-        System.out.println("   Длина интервала: " + range2 + " = " + range2.lengthRange());
-
-
-        // Проверяем функцию вычисления интервала-пересечения двух интервалов
-
-        Range resultRange = range1.crossingRange(range2);
-        System.out.println(resultRange == null ? "3. Интервалы не пересекаются" : "3. Интервал пересечения отрезков" + range1 + " и " + range2 + " = " + resultRange);
-
-        // Проверяем функцию объединения двух интервалов
-
-        Range[] mergedRange = range1.mergerRange(range2);
+        // 3.Проверяем функцию объединения двух интервалов
+        Range[] mergedRange = range1.unionRange(range2);
 
         for (int i = 0; i <= mergedRange.length - 1; i++) {
-            System.out.println("4. Результатом объединения интервалов " + range1 + " и " + range2 + " является интервал " + mergedRange[i]);
+            System.out.println("3. Объединение интервалов: " + mergedRange[i]);
         }
 
+        // 4. Проверяем функцию вычисления интервала-пересечения двух интервалов
+        Range resultRange = range1.crossingRange(range2);
+        System.out.println(resultRange == null ? "4. Интервалы не пересекаются" : "4. Пересечение: " + resultRange);
+
+        //5. Проверяем функцию разности range1 - range2
+        Range[] resultDifference = range1.difference(range2);
+        if (resultDifference == null) {
+            System.out.println("5. Разности A - B не существует ");
+        } else {
+            System.out.print("5. Разность A - B: ");
+            for (int i = 0; i <= resultDifference.length - 1; i++) {
+                System.out.print(resultDifference[i]);
+            }
+        }
     }
 }
+
