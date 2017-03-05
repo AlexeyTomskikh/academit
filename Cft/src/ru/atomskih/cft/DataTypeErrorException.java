6e083dc34509b26a10d248e11f5cb43884281d3b
AlexeyTomskikh <alexey.tomskih@gmail.com>
2017-03-05 23:39:44 +0700

    Переименовал исключение при ошибке в типе данных

---
+++ Cft/src/ru/atomskih/cft/DataTypeErrorException.java
@@ -0,0 +1,8 @@
+package ru.atomskih.cft;
+
+public class DataTypeErrorException extends Exception {
+
+    public DataTypeErrorException() {
+        super();
+    }
+}
--- Cft/src/ru/atomskih/cft/UserException.java
+++
@@ -1,8 +0,0 @@
-package ru.atomskih.cft;
-
-public class UserException extends Exception {
-
-    public UserException() {
-        super();
-    }
-}
